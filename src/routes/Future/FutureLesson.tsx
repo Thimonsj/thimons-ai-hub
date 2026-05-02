25e90371284f93df11b8856ab4f1615ee32c4b3d
Thimons J <thimonsjennah@gmail.com>
2026-05-02 21:41:11 +0400

    fix content paths

--- src/routes/Future/FutureLesson.tsx
+++ src/routes/Future/FutureLesson.tsx
@@ -1,5 +1,5 @@
 import LessonLayout from '../../src/components/LessonLayout';
-import lesson from '../../src/content/futureLesson1';
+import lesson from '../../content/futureLesson1';
 
 export default function FutureLesson() {
   return <LessonLayout title="Humanoid Motion Control" lesson={lesson} />;
--- src/routes/Innovators/InnovatorsLesson.tsx
+++ src/routes/Innovators/InnovatorsLesson.tsx
@@ -1,5 +1,5 @@
 import LessonLayout from '../../src/components/LessonLayout';
-import lesson from '../../src/content/innovatorsLesson1';
+import lesson from '../../content/innovatorsLesson1';
 
 export default function InnovatorsLesson() {
   return <LessonLayout title="From Blocks to Python" lesson={lesson} />;
